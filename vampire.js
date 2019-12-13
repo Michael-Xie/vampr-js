efcce755dfcf04eec40a6e7ffc1ce3ea856e08ca
Michael-Xie <xiemicha@gmail.com>
2019-12-13 21:49:32 +0000

    Completed first set of functions

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampire = 0;
+    let currentVampire = this;
+
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampire++;
+    }
+
+    return numberOfVampire;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -37,6 +48,33 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    let firstVamp = this;
+    let secondVamp = vampire;
+
+    let lowestVampire = firstVamp.numberOfVampiresFromOriginal < secondVamp.numberOfVampiresFromOriginal? secondVamp: firstVamp;
+    let higherVampire = lowestVampire === firstVamp? secondVamp: firstVamp;
+
+    while(lowestVampire.numberOfVampiresFromOriginal) {
+      // Check if whether the two are the same vamp - same branch
+      if (lowestVampire.name === higherVampire.name) {
+        return lowestVampire;
+      }
+      // Check if higherVamp is creator of lowerVamp - same branch
+      if (lowestVampire.creator && lowestVampire.creator.name === higherVampire.name) {
+        return higherVampire;
+      } 
+      // Check if higherVamp and lowerVamp are siblings
+      if (lowestVampire.creator && higherVampire.creator && lowestVampire.creator.name === higherVampire.creator.name) {
+        return lowestVampire.creator;
+      } 
+      lowestVampire = lowestVampire.creator;
+
+      // Keep higherVamp higher than lowestVamp
+      if (higherVampire.creator && lowestVampire.numberOfVampiresFromOriginal === higherVampire.numberOfVampiresFromOriginal) {
+        higherVampire = higherVampire.creator;
+      }
+    }
+    return undefined;
   }
 }
 
